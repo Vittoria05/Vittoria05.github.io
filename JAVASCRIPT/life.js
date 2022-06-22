4edbb49762cb8c48abaf6f0e03d33a6cbbe39615
Vittoria05 <105248422+Vittoria05@users.noreply.github.com>
2022-06-22 15:16:55 +0100

    Add files via upload

---
+++ JAVASCRIPT/life.js
@@ -0,0 +1,7 @@
+let fullName = document.getElementById('name');
+let height = document.getElementById('height');
+let country = document.getElementById('country');
+
+fullName.innerHTML ='Name: Ogundele Vittoria'
+height.innerHTML ='Height: 5.9ft'
+country.innerHTML ='Country: Nigeria'
